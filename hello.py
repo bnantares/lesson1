c224021995aab09084b03c86a0e7ae475ca12c7a
Anton Petrov <ant8151@yandex.ru>
2020-09-05 21:42:09 +0300

    Первая версия

---
+++ hello.py
@@ -0,0 +1,4 @@
+name = 'Vikhtoria'
+user_info = {'first_name': name, 'last_name': 'Petroff'}
+
+print(user_info['last_name'])
---
+++ ld.py
@@ -0,0 +1,4 @@
+def get_summ(one, two, delimiter = '&'):
+    print(f"{one} {delimiter} {two}")
+
+get_summ('Learn'.capitalize(), 'python'.capitalize())    
